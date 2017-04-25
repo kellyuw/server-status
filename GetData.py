863893b79445b1961bb0385c9ce71fa9472f8df6
kellyuw <kelly89@uw.edu>
2017-04-25 12:36:10 -0700

    Adding basic ping and plot scripts

---
+++ GetData.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from datetime import datetime
+import os
+
+epoch = datetime.utcfromtimestamp(0)
+project_dir = "/Users/kelly89/Projects/ServerStatus/"
+server_list = project_dir + "MachineList.txt"
+t = datetime.now()
+ofile = project_dir + "logfiles/" + datetime.strftime(t,"%Y%m%d-%H%M%S") + ".csv"
+print(ofile)
+
+def unix_time (dt):
+    return (dt - epoch).total_seconds()
+
+def get_ping_results():
+    servers = [s.strip("\n") for s in open(server_list).readlines()]
+    results = []
+
+    for s in servers:
+        hostname = s + ".ibic.washington.edu"
+        r = ping_server(hostname)
+        results += [[r, s, t]]
+    return(results)
+
+def ping_server(n):
+    print 'pinging ' + str(n)
+    return "success" if os.system("ping -c 1 -t 3 " + n) == 0 else "fail"
+
+data = get_ping_results()
+df = pd.DataFrame.from_records(data, columns = ['result','server','time'])
+df['time_since_epoch'] = int((t - epoch).total_seconds())
+df.to_csv(ofile, sep = ',', index = False, header = True)
---
+++ PlotData.py
@@ -0,0 +1,20 @@
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+import glob as glob
+import pandas as pd
+from datetime import datetime
+
+project_dir = "/Users/kelly89/Projects/ServerStatus/"
+ofile = project_dir + datetime.strftime(datetime.now(),"%Y%m%d-%H%M%S") + ".png"
+
+all_data = pd.concat([pd.read_csv(f) for f in glob.glob(project_dir + "logfiles/*.csv")], axis = 0)
+all_data['adj_time'] = all_data['time_since_epoch'] - all_data['time_since_epoch'].min()
+
+redgreen = ["#2ecc71","#e74c3c"]
+sns.set_palette(redgreen)
+
+#Plot results in seaborn
+f, ax = plt.subplots(figsize=(11, 9))
+sns_plot = sns.stripplot(x="adj_time", y="server", hue="result", data=all_data)
+plt.savefig(ofile)
